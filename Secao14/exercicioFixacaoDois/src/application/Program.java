1bfe6c355bbc6053aabdb83c05a58450d5417102
igorpompeo <igor.pompeo1@ibm.com>
2020-04-30 18:07:56 -0300

    L'Ezercicio di fissazione fato Sulla tasse che il/la dipendente e azienda

---
+++ Secao14/exercicioFixacaoDois/src/application/Program.java
@@ -0,0 +1,56 @@
+package application;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+import java.util.Scanner;
+
+import entities.Company;
+import entities.Individual;
+import entities.TaxPayer;
+
+public class Program {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		List<TaxPayer> list = new ArrayList<>();
+		
+		System.out.println("Enter the number of tax payers: ");
+		int n = sc.nextInt();
+		
+		for (int i = 1; i <= n; i++) {
+			System.out.println("Tax payers #" + i + " data: ");
+			System.out.print("Individual or company (i/c)? ");
+			char type = sc.next().charAt(0);
+			System.out.print("Name: ");
+			sc.nextLine();
+			String name = sc.nextLine();
+			System.out.print("Anual income: ");
+			double income = sc.nextDouble();
+			if (type == 'i') {
+				System.out.print("Health expenditures: ");
+				double healthExpenditures = sc.nextDouble();
+				list.add(new Individual(name, income, healthExpenditures));
+			} else {
+				System.out.print("Number of employees: ");
+				int numberOfEmployees = sc.nextInt();
+				list.add(new Company(name, income, numberOfEmployees));
+			}
+		}
+		double sum = 0.0;
+		System.out.println();
+		System.out.println("TAXES PAID:");
+		for (TaxPayer taxPayer : list) {
+			double tax = taxPayer.tax();
+			System.out.println(taxPayer.getName() + ": $ " + String.format("%.2f", tax));
+			sum += tax;
+		}
+		System.out.println();
+		System.out.println("TOTAL TAXES: $ " + String.format("%.2f", sum));
+		sc.close();
+	}
+
+}
