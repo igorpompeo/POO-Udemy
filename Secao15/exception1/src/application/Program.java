55fe64e879221d091b0e1bc15326fe3fb4ed3126
igorpompeo <igor.pompeo1@ibm.com>
2020-07-24 20:23:38 -0300

    Projeto recriado no eclipse - VSCode quebrando

--- Secao15/exception1/src/application/Program.java
+++ Secao15/exception1/src/application/Program.java
@@ -1,15 +1,17 @@
-package app;
+package application;
 
-import java.sql.Date;
+import java.util.Date;
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Scanner;
 
 import model.entities.Reservation;
 
-public class Program1 {
-    public static void main(final String[] args) throws ParseException {
-        
-        Scanner sc = new Scanner(System.in);
+public class Program {
+
+	public static void main(String[] args) throws ParseException {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
         SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
         
         System.out.print("Room number: ");
@@ -47,5 +49,6 @@ public class Program1 {
         }
 
         sc.close();
-    }
+	}
+
 }
