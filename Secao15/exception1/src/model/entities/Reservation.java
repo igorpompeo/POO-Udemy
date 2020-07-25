7f6fd5fdbc3072209d3eaab0bb002fef4357a7fc
igorpompeo <igor.pompeo1@ibm.com>
2020-07-24 21:13:30 -0300

    Bas solution

--- Secao15/exception1/src/model/entities/Reservation.java
+++ Secao15/exception1/src/model/entities/Reservation.java
@@ -5,6 +5,7 @@ import java.util.Date;
 import java.util.concurrent.TimeUnit;
 
 public class Reservation {
+	
 		private Integer roomNumber;
 	    private Date checkIn, checkOut;
 
@@ -37,18 +38,29 @@ public class Reservation {
 	        return TimeUnit.DAYS.convert(diff, TimeUnit.MILLISECONDS);
 	    }
 
-	    public void updateDates(Date checkIn, Date checkOut){
+	    public String updateDates(Date checkIn, Date checkOut){
+	    	
+	    	Date now = new Date();
+
+            if (checkIn.before(now) || checkOut.before(now)){
+                return "Reservation dates for update must be future";
+            } if (!checkOut.after(checkIn)) {
+               return "Check-out date must be after check-in date";
+            }
+            
 	        this.checkIn = checkIn;
 	        this.checkOut = checkOut;
+	        
+	        return null;
 	    }
 
 	    @Override
 	    public String toString(){
 	        return "Room"
 	            + roomNumber
-	            + ", check-in"
+	            + ", check-in "
 	            + sdf.format(checkIn)
-	            + ", check-out"
+	            + ", check-out "
 	            + sdf.format(checkOut)
 	            + ", "
 	            + duration()
